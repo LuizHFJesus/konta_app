ef04c65520b9c3986d69df78d4fe8c3be5c93721
Luiz Henrique Firmino de Jesus <l176204@dac.unicamp.br>
2025-09-26 00:03:55 -0300

    feat(home): create base screen with BNB and navigation

--- lib/app/navigation/app_routes.dart
+++ lib/app/navigation/app_routes.dart
@@ -1,4 +1,7 @@
 class AppRoutes {
   static const String auth = '/auth';
   static const String home = '/';
+  static const String reports = '/reports';
+  static const String reminder = '/reminder';
+  static const String profile = '/profile';
 }
