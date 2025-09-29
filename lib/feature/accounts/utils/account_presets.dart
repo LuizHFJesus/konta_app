d450be363eb169c8afd510c53a10c880bbeb4bc0
Luiz Henrique Firmino de Jesus <l176204@dac.unicamp.br>
2025-09-29 14:28:04 -0300

    feat(accounts): define predefined icons and colors for accounts

---
+++ lib/feature/accounts/utils/account_presets.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AccountPresets {
+  static const List<Color> predifinedColors = [
+    Color(0xFFF1DD1E),
+    Color(0xFFF5B426),
+    Color(0xFFF2832A),
+    Color(0xFFF0522B),
+    Color(0xFFEB2D37),
+    Color(0xFF9E5CA4),
+    Color(0xFF6F58A6),
+    Color(0xFF4959A6),
+    Color(0xFF4570B5),
+    Color(0xFF1AA7AE),
+    Color(0xFF4EB84C),
+    Color(0xFF95C940),
+  ];
+
+  static const List<IconData> predifinedIcons = [
+    Icons.receipt_long,
+    Icons.wallet,
+    Icons.savings,
+    Icons.credit_card,
+    Icons.account_balance_wallet,
+    Icons.percent,
+    Icons.monetization_on,
+    Icons.attach_money,
+    Icons.bar_chart,
+    Icons.post_add_sharp,
+    Icons.swap_horiz,
+    Icons.local_atm,
+  ];
+}
