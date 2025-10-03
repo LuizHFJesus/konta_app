3b164e173a87742418b90dbce5d8e103277b3c7c
Luiz Henrique Firmino de Jesus <l176204@dac.unicamp.br>
2025-10-02 21:02:51 -0300

    feat(transactions): define transaction model (entity)

---
+++ lib/feature/transaction/domain/transaction.dart
@@ -0,0 +1,45 @@
+enum TransactionType { income, expense }
+
+class Transaction {
+  final String id;
+  final TransactionType type;
+  final double amount;
+  final DateTime date;
+  final String? description;
+  final String categoryId;
+  final String accountId;
+
+  const Transaction({
+    required this.id,
+    required this.type,
+    required this.amount,
+    required this.date,
+    this.description,
+    required this.categoryId,
+    required this.accountId,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'type': type,
+      'amount': amount,
+      'date': date,
+      'description': description,
+      'categoryId': categoryId,
+      'accountId': accountId,
+    };
+  }
+
+  factory Transaction.fromJson(Map<String, dynamic> map) {
+    return Transaction(
+      id: map['id'] as String,
+      type: map['type'] as TransactionType,
+      amount: map['amount'] as double,
+      date: map['date'] as DateTime,
+      description: map['description'] as String?,
+      categoryId: map['categoryId'] as String,
+      accountId: map['accountId'] as String,
+    );
+  }
+}
