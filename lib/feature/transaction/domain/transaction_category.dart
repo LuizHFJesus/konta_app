6f80f9e891ae1a4308ec6c46d020abf6f6f5c3a7
Luiz Henrique Firmino de Jesus <l176204@dac.unicamp.br>
2025-10-02 21:15:39 -0300

    feat(transactions): define transaction category model (entity)

---
+++ lib/feature/transaction/domain/transaction_category.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:konta_app/feature/transaction/domain/transaction.dart';
+
+class TransactionCategory {
+  final String id;
+  final TransactionType type;
+  final String name;
+  final int iconCodePoint;
+  final int colorValue;
+
+  TransactionCategory({
+    required this.id,
+    required this.type,
+    required this.name,
+    required this.iconCodePoint,
+    required this.colorValue,
+  });
+
+  Color get color => Color(colorValue);
+
+  IconData get icon => IconData(iconCodePoint, fontFamily: 'MaterialIcons');
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'type': type,
+      'name': name,
+      'iconCodePoint': iconCodePoint,
+      'colorValue': colorValue,
+    };
+  }
+
+  factory TransactionCategory.fromJson(Map<String, dynamic> map) {
+    return TransactionCategory(
+      id: map['id'] as String,
+      type: map['type'] as TransactionType,
+      name: map['name'] as String,
+      iconCodePoint: map['iconCodePoint'] as int,
+      colorValue: map['colorValue'] as int,
+    );
+  }
+}
